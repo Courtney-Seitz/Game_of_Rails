b399fa7973132b736346a50ce8aec4ac7b3dce56
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-08-02 22:57:29 -0400

    stopping here

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,40 +1,40 @@
 class HousesController < ApplicationController
 
-def index
-  @houses = House.all
-end
+  def index
+    @houses = House.all
+  end
 
-def show
-  @house = House.find(params[:id])
-end
+  def show
+    @house = House.find(params[:id])
+  end
 
-def new
-  @house = House.new
-end
+  def new
+    @house = House.new
+  end
 
-def create
-  @house = House.create!(house_params)
-  redirect_to @house
-end
+  def create
+    @house = House.create!(house_params)
+    redirect_to @house
+  end
 
-def edit
-  @house = House.find(params[:id])
-end
+  def edit
+    @house = House.find(params[:id])
+  end
 
-def update
-  @house = House.find(params[:id])
-  redirect_to @house
-end
+  def update
+    @house = House.find(params[:id])
+    redirect_to @house
+  end
 
-def destroy
-   @house = House.find(params[:id])
-   @house.destroy
-   redirect_to houses_path
- end
+  def destroy
+    @house = House.find(params[:id])
+    @house.destroy
+    redirect_to houses_path
+  end
 
-private
-def house_params
-  params.require(:house).permit(:name)
-end
+  private
+  def house_params
+    params.require(:house).permit(:name)
+  end
 
 end
